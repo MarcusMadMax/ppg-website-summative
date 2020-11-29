6b1db4dc6fe5a0c320f7537d00485cddddfbb653
MarcusMadMax <marcuszillner@icloud.com>
2020-11-29 19:50:04 +1300

    finished for now

--- PPG_Webpage_Summative/assets/js/main.js
+++ PPG_Webpage_Summative/assets/js/main.js
@@ -1,10 +1,11 @@
 const bars = document.querySelector('.bars')
 const nav = document.querySelector('.nav-links')
 const navLinks = document.querySelectorAll('.nav-links li')
-const navBarLogo = document.querySelector('.logo')
+const navBarLogo = document.querySelector('li a img')
 const navBar = document.querySelector('nav')
 const navLinkA = document.querySelectorAll('.nav-links li a')
 const address = document.querySelector('.contact-city')
+const taxControlButtons = document.querySelector('.taxonomy-control-container-buttons')
 
 const navSlide = () => {
     bars.addEventListener('click', () => {
@@ -22,63 +23,40 @@ const navSlide = () => {
 
         //Bars Animation
         bars.classList.toggle('toggle')
-
-        //Logo Animation
-        navBarLogo.classList.toggle('navBarLogoReize')
+        if ($('.what-we-do-page').length > 0) {
+        //Get rid of taxonomy
+        taxControlButtons.classList.toggle('no-tax')
+        }
     })
 
 }
 navSlide()
 
+w = document.documentElement.clientWidth || document.body.clientWidth || window.innerWidth;
+var targetWidth = 1200;
+if (w >= targetWidth) {
+    // When scrolling NavBar changes in Desktop View
+    window.onscroll = function () { scrollFunction() };
 
-// When scrolling NavBar changes in Desktop View
-window.onscroll = function () { scrollFunction() };
+    scrollFunction = () => {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            navBar.classList.add('style-nav-bar-in')
+            navBar.classList.remove('style-nav-bar-out')
+            navBarLogo.classList.add('style-logo-in')
+            navBarLogo.classList.remove('style-logo-out')
 
-scrollFunction = () => {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navBar.classList.add('style-nav-bar-in')
-        navBar.classList.remove('style-nav-bar-out')
-        navBarLogo.classList.add('style-logo-in')
-        navBarLogo.classList.remove('style-logo-out')
-        // navLinksA.classList.add('style-links-in')
-        // navLinksA.classList.remove('style-links-out')
-
-    } else {
-        navBar.classList.remove('style-nav-bar-in')
-        navBar.classList.add('style-nav-bar-out')
-        navBarLogo.classList.remove('style-logo-in')
-        navBarLogo.classList.add('style-logo-out')
-        // navLinksA.classList.remove('style-links-in')
-        // navLinksA.classList.add('style-links-out')
-
+        } else {
+            navBar.classList.remove('style-nav-bar-in')
+            navBar.classList.add('style-nav-bar-out')
+            navBarLogo.classList.remove('style-logo-in')
+            navBarLogo.classList.add('style-logo-out')
+        }
     }
-
-    //Animate Links
-    // navLinksA.forEach((link, index) => {
-    //     if (link.style.animation) {
-    //         link.style.animation = ''
-    //     } else {
-    //         link.style.animation = ('style-links-in')
-    //     }
-    // })
-
-    // navLinksA.forEach() = () => {
-    //     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    //         navLinksA.classList.add('style-links-in')
-    //         navLinksA.classList.remove('style-links-out')
-    //     } else {
-    //         navLinksA.classList.remove('style-links-in')
-    //         navLinksA.classList.add('style-links-out')
-    //     }
-    // }
-
 }
-
-
 
 //Boat is moving in
 $(function () {
-    if($('.home-page .main-section').length > 0){
+    if ($('.home-page .main-section').length > 0) {
         var anime1 = anime({
             targets: '.main-section-am-cup-boat',
             translateX: [0, '-50vw'],
@@ -86,10 +64,10 @@ $(function () {
             duration: 1000,
             autoplay: false,
         })
-    
+
         var mainS = $('.home-page .main-section').offset().top
         $(document).on('scroll', function () {
-    
+
             var scrollTop = $(document).scrollTop()
             var progress = (scrollTop - (mainS - 500)) / 900
             anime1.seek(anime1.duration * progress)
@@ -98,100 +76,100 @@ $(function () {
 
 
     //Taxonomy
-    if($('.what-we-do-page .taxonomy-control-container .container').length > 0){
+    if ($('.what-we-do-page .taxonomy-control-container .container').length > 0) {
         var mixer = mixitup('.what-we-do-page .taxonomy-control-container .container');
     }
 })
 
-if($('#firstName #lastName #email .form').length > 0){
-//Form
-//Event handler
-function checkFilledIn() {
-    var sValue = this.value
-    var oAlphabeticExp = /^[A-Za-z]*$/
-    var bResult = oAlphabeticExp.test(sValue)
-    var isValid = false
+if ($('#firstName #lastName #email .form').length > 0) {
+    //Form
+    //Event handler
+    function checkFilledIn() {
+        var sValue = this.value
+        var oAlphabeticExp = /^[A-Za-z]*$/
+        var bResult = oAlphabeticExp.test(sValue)
+        var isValid = false
 
-    if (sValue == '') {
-        this.style.backgroundColor = 'red'
-        this.style.color = 'red'
-        this.innerHTML = 'Please fill in'
-    }else{
-        this.style.backgroundColor = 'green'
-        this.style.color = 'green'
-        this.innerHTML = 'Thank you'
-        isValid = true
+        if (sValue == '') {
+            this.style.backgroundColor = 'red'
+            this.style.color = 'red'
+            this.innerHTML = 'Please fill in'
+        } else {
+            this.style.backgroundColor = 'green'
+            this.style.color = 'green'
+            this.innerHTML = 'Thank you'
+            isValid = true
+        }
+        return isValid
     }
-    return isValid
-}
 
-function checkLetters() {
-    var sValue = this.value
-    var oAlphabeticExp = /^[A-Za-z]*$/
-    var bResult = oAlphabeticExp.test(sValue)
-    var isValid = false
+    function checkLetters() {
+        var sValue = this.value
+        var oAlphabeticExp = /^[A-Za-z]*$/
+        var bResult = oAlphabeticExp.test(sValue)
+        var isValid = false
 
-    if (sValue == '') {
-        this.parentNode.previousElementSibling.style.color = '#FA0029'
-        this.parentNode.previousElementSibling.innerHTML = 'Please fill in'
-    } else if(bResult == false){
-        this.parentNode.previousElementSibling.style.color = '#FA0029'
-        this.parentNode.previousElementSibling.innerHTML = 'Only letters'
-    }else{
-        this.parentNode.previousElementSibling.style.color = 'green'
-        this.parentNode.previousElementSibling.innerHTML = 'Thanks'
-        isValid = true
+        if (sValue == '') {
+            this.parentNode.previousElementSibling.style.color = '#FA0029'
+            this.parentNode.previousElementSibling.innerHTML = 'Please fill in'
+        } else if (bResult == false) {
+            this.parentNode.previousElementSibling.style.color = '#FA0029'
+            this.parentNode.previousElementSibling.innerHTML = 'Only letters'
+        } else {
+            this.parentNode.previousElementSibling.style.color = 'green'
+            this.parentNode.previousElementSibling.innerHTML = 'Thanks'
+            isValid = true
+        }
+        return isValid
     }
-    return isValid
-}
 
-function checkEmail() {
-    var sEmail = this.value
-    var oAlphabeticExp = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/
-    var bResult = oAlphabeticExp.test(sEmail)
-    var isValid = false
+    function checkEmail() {
+        var sEmail = this.value
+        var oAlphabeticExp = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/
+        var bResult = oAlphabeticExp.test(sEmail)
+        var isValid = false
 
-    if (sEmail == '') {
-        this.parentNode.previousElementSibling.style.color = '#FA0029'
-        this.parentNode.previousElementSibling.innerHTML = 'Please fill in'
-    } else if(bResult == false){
-        this.parentNode.previousElementSibling.style.color = '#FA0029'
-        this.parentNode.previousElementSibling.innerHTML = 'Not a valid e-mail'
-    }else{
-        this.parentNode.previousElementSibling.style.color = 'green'
-        this.parentNode.previousElementSibling.innerHTML = 'Thanks'
-        isValid = true
+        if (sEmail == '') {
+            this.parentNode.previousElementSibling.style.color = '#FA0029'
+            this.parentNode.previousElementSibling.innerHTML = 'Please fill in'
+        } else if (bResult == false) {
+            this.parentNode.previousElementSibling.style.color = '#FA0029'
+            this.parentNode.previousElementSibling.innerHTML = 'Not a valid e-mail'
+        } else {
+            this.parentNode.previousElementSibling.style.color = 'green'
+            this.parentNode.previousElementSibling.innerHTML = 'Thanks'
+            isValid = true
+        }
+        return isValid
     }
-    return isValid
-}
 
-function checkAll(e){
-    e.preventDefault()
-    var isFirstName = checkLetters.call(oFirstName)
-    var isLastName = checkLetters.call(oLastName)
-    var isEmail = checkEmail.call(oEmail)
-    var isAllVaild =  isFirstName && isEmail && isLastName
+    function checkAll(e) {
+        e.preventDefault()
+        var isFirstName = checkLetters.call(oFirstName)
+        var isLastName = checkLetters.call(oLastName)
+        var isEmail = checkEmail.call(oEmail)
+        var isAllVaild = isFirstName && isEmail && isLastName
 
-    if(isAllVaild == false){
-        e.preventDefault() 
+        if (isAllVaild == false) {
+            e.preventDefault()
+        }
     }
- }
- 
 
 
-//Main programm
-var oFirstName = document.querySelector('#firstName')
-oFirstName.addEventListener('blur', checkLetters)
 
-var oLastName = document.querySelector('#lastName')
-oLastName.addEventListener('blur', checkLetters)
+    //Main programm
+    var oFirstName = document.querySelector('#firstName')
+    oFirstName.addEventListener('blur', checkLetters)
 
-var oEmail = document.querySelector('#email')
-oEmail.addEventListener('blur', checkEmail)
+    var oLastName = document.querySelector('#lastName')
+    oLastName.addEventListener('blur', checkLetters)
+
+    var oEmail = document.querySelector('#email')
+    oEmail.addEventListener('blur', checkEmail)
 
 
-var oForm = document.querySelector('.form')
-oForm.addEventListener('submit', checkAll)
+    var oForm = document.querySelector('.form')
+    oForm.addEventListener('submit', checkAll)
 
 }
 
